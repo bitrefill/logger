6cfc6890f40cbfe3825093449cd0a08cef27c1da
Maxim Orlov <maximorl@gmail.com>
2018-03-28 14:36:01 +0700

    Add CLI tool for piping heroku livetail

---
+++ cli.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+const split = require('split2');
+const pump = require('pump');
+const through = require('through2');
+const program = require('commander');
+const chalk = require('chalk');
+const pretty = require('pino').pretty();
+
+program
+    .description('Pipe heroku live tail logs into a readable output')
+    .option('-i, --include [include]', 'Comma-delimited list of properties to include. Defaults to all')
+    .parse(process.argv);
+
+let props = [];
+if (program.include) {
+    props = program.include.split(',');
+}
+
+const transformer = through.obj((chunk, enc, cb) => {
+    const regex = /^.*?:\s({.*})$/;
+
+    // Non-JSON logs
+    if (!regex.test(chunk)) {
+        const [, herokuSyslog, message] = chunk.match(/^(.*?]:)(\s.*)$/);
+        return cb(null, `${chalk.green(herokuSyslog)}${message}\n`);
+    }
+
+    // Parse JSON
+    const parsed = JSON.parse(chunk.match(regex)[1]);
+
+    // Cherry picked default properties
+    const defaultProps = {
+        time: parsed.time,
+        level: parsed.level,
+        name: parsed.name,
+        v: parsed.v,
+        msg: parsed.msg,
+        namespace: parsed.namespace,
+    };
+
+    const extraProps = !props.length
+        // Include all properties if user hasn't chosen any
+        ? parsed
+        // Included properties by user
+        : props.reduce((acc, prop) => ({
+            ...acc,
+            [prop]: parsed[prop],
+        }), {});
+
+    // Stringify JSON
+    const stringified = JSON.stringify({ ...defaultProps, ...extraProps });
+
+    return cb(null, `${stringified}\n`);
+});
+
+// Pipeline steps
+pump(
+    process.stdin, // Take everything that is piped in
+    split(), // Split stream so we have a line per chunk
+    transformer, // Apply our own transformer
+    pretty, // Pretty print JSON lines through Pino
+    process.stdout, // Output everything to stdout
+);
